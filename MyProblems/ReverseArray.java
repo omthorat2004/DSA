8029a0ab0ff7425231a4d8f20dd1339b3c541a5f
Om Thorat <154694985+omthorat2004@users.noreply.github.com>
2025-10-07 10:26:48 -0700

    Update ReverseArray.java

--- MyProblems/ReverseArray.java
+++ MyProblems/ReverseArray.java
@@ -1,21 +1,5 @@
 public class ReverseArray {
 
-  public static void main(String[] args) {
-    int[] arr = { 1, 2, 3, 4 };
-    int n = arr.length;
-
-        
-  
-
-    System.out.println(mid);
-sss
-    int i = 0;
-    while (i < mid) {
-      int temp = arr[i];
-      arr[i] = arr[n - 1 - i];
-      arr[n - 1 - i] = temp;
-      i++;
-    }
 
     for (int j = 0; j < n; j++) {
       System.out.println(arr[j]);
